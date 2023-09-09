1647fd20ffbb6e93cf54d442fb25040119aa35ee
starrleyet <starrleyet@gmail.com>
2023-09-09 11:48:31 +0800

    新增项目

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+    public void PlayerMove() {
+        float h = Input.GetAxis("Horizontal") * Time.deltaTime *3;
+        float v = Input.GetAxis("Vertical") * Time.deltaTime * 3;
+        transform.Translate(new Vector3(h, 0, v));
+    }
+}
