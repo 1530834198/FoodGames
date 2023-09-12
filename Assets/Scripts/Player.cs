7e0463dd750093a1d8c83c82e791b63341e3402d
3240765224 <1530834198@qq.com>
2023-09-12 15:43:27 +0800

    修复角色移动抖动
    抖动和刷新的帧率有关系

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,52 +1,110 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Player : MonoBehaviour
+namespace Cinemachine.Examples
 {
-    private Animator Anim;
-    private int speed = 3;  //人物移动速度
-    Camera mainCamera;
-    public float turnSpeed=5; //人物转向速度
-    // Start is called before the first frame update
-    void Awake()
+
+    [AddComponentMenu("")] // Don't display in add component menu
+    public class Player : MonoBehaviour
     {
-        Anim = GetComponent<Animator>();
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        mainCamera = Camera.main;
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
+        public bool useCharacterForward = false;//用于确定角色是否应该根据自己的前方方向移动
+        public bool lockToCameraForward = false;//可能用于锁定角色的移动方向到摄像机的前方。
+        public float turnSpeed = 10f;//角色的旋转速度
+
+        private float turnSpeedMultiplier;//调整旋转速度
+        private float speed = 0f;//角色的移动速度
+        private float direction = 0f;//表示角色的方向
+
+        private Animator anim;
+        private Vector3 targetDirection;//一个Vector3，表示角色应该面向的方向
+        private Vector2 input;//存储水平和垂直输入以控制角色的移动
+        private Quaternion freeRotation;//用于控制自由旋转
+        private Camera mainCamera;//主摄像机的引用
+        private float velocity;//平滑地处理角色的速度变化
+
+
+        // Use this for initialization
+        void Start()
+        {
+            anim = GetComponent<Animator>();
+            mainCamera = Camera.main;
+            //鼠标隐藏
+            Cursor.visible = false;
+            //鼠标锁定
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        // Update is called once per frame
+        /**
+         * 角色的移动和旋转，计算速度并更新动画参数
+         */
+        void FixedUpdate()
+        {
+            input.x = Input.GetAxis("Horizontal");
+            input.y = Input.GetAxis("Vertical");
+
+            // set speed to both vertical and horizontal inputs
+            if (useCharacterForward)
+                speed = Mathf.Abs(input.x) + input.y;
+            else
+                speed = Mathf.Abs(input.x) + Mathf.Abs(input.y);
+
+            speed = Mathf.Clamp(speed, 0f, 1f);
+            speed = Mathf.SmoothDamp(anim.GetFloat("Speed"), speed, ref velocity, 0.1f);
+            anim.SetFloat("Speed", speed);
+
+            if (input.y < 0f && useCharacterForward)
+                direction = input.y;
+            else
+                direction = 0f;
+
+            anim.SetFloat("Direction", direction);
+
+           
+
+            // Update target direction relative to the camera view (or not if the Keep Direction option is checked)
+            UpdateTargetDirection();
+            if (input != Vector2.zero && targetDirection.magnitude > 0.1f)
+            {
+                Vector3 lookDirection = targetDirection.normalized;
+                freeRotation = Quaternion.LookRotation(lookDirection, transform.up);
+                var diferenceRotation = freeRotation.eulerAngles.y - transform.eulerAngles.y;
+                var eulerY = mainCamera.transform.eulerAngles.y;
+
+                if (diferenceRotation < 0 || diferenceRotation > 0) eulerY = freeRotation.eulerAngles.y;
+                var euler = new Vector3(0, eulerY, 0);
+                
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(euler), turnSpeed * turnSpeedMultiplier * Time.deltaTime);
+            }
+        }
+
+        /**
+         * 更新目标方向，该方向相对于摄像机视图来决定（或者根据 useCharacterForward 标志）。这个方法影响角色的移动方向。
+         */
+        public virtual void UpdateTargetDirection()
+        {
+            if (!useCharacterForward)
+            {
+                turnSpeedMultiplier = 1f;
+                var forward = mainCamera.transform.TransformDirection(Vector3.forward);
+                forward.y = 0;
+        
+                //get the right-facing direction of the referenceTransform
+                var right = mainCamera.transform.TransformDirection(Vector3.right);
+        
+                // determine the direction the player will face based on input and the referenceTransform's right and forward directions
+                targetDirection = input.x * right + input.y * forward;
+            }
+            else
+            {
+                turnSpeedMultiplier = 0.2f;
+                var forward = transform.TransformDirection(Vector3.forward);
+                forward.y = 0;
+        
+                //get the right-facing direction of the referenceTransform
+                var right = transform.TransformDirection(Vector3.right);
+                targetDirection = input.x * right + Mathf.Abs(input.y) * forward;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        PlayerMove();
-        Charachter();
-    }
-    public void PlayerMove() {
-        float h = Input.GetAxis("Horizontal") * Time.deltaTime *3;
-        float v = Input.GetAxis("Vertical") * Time.deltaTime * 3;
-        Vector3 move = new Vector3(h, 0, v);    //获取坐标系
-        
-    }
-    /**
-     * 角色的移动动画
-     */
-    public void Charachter() {
-        //判断我keyDown的按键
-        bool isWalking = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D);
-        //触发动画
-        Anim.SetBool("walk", isWalking);
-    }
-    private void FixedUpdate()
-    {
-        //获取终点方向Y轴数据
-        float playerCamera = mainCamera.transform.rotation.eulerAngles.y;
-        //球形差值（起始方向,终点方向,速度）
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, playerCamera, 0), turnSpeed *Time.fixedDeltaTime);
-    }
 }
