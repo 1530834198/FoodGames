98a3e3bb93390762ff49f6863a48433bd29bea3a
3240765224 <1530834198@qq.com>
2023-09-13 16:21:16 +0800

    新增菜单背景音乐和音乐设置

--- Assets/Scripts/fm.cs
+++ Assets/Scripts/fm.cs
@@ -5,10 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class fm : MonoBehaviour
 {
+    public GameObject bgm;
+    public GameObject bgmprefab;
+
+    public GameObject bgmPanel;
     // Start is called before the first frame update
     void Start()
     {
-        
+        bgm = GameObject.FindGameObjectWithTag("menuBgm");//先去找有没有bgm
+        //没有实例化一个
+        if (bgm == null)
+        {
+            bgm = Instantiate(bgmprefab);
+        }
     }
 
     // Update is called once per frame
@@ -19,5 +28,45 @@ public class fm : MonoBehaviour
     public void SatrtGame()
     {
         SceneManager.LoadScene(1);
+    }
+    
+    /**
+     * bgm静音
+     */
+    public void OnToggle(bool isOn)
+    {
+        if (!isOn)//没选择
+        {
+            bgm.GetComponent<AudioSource>().Play();
+        }
+        else
+        {
+            //选中了
+            bgm.GetComponent<AudioSource>().Pause();
+        }
+    }
+    
+    /**
+     * 关闭音量设置panel
+     */
+    public void OnCloseBgmBtnClick()
+    {
+        bgmPanel.SetActive(false);
+    }
+    
+    /**
+     * 开启音量设置panel
+     */
+    public void OnstartBgmBtnClick()
+    {
+        bgmPanel.SetActive(true);
+    }
+    
+    /**
+     * 音量控制
+     */
+    public void OnSliderChange(float value)
+    {
+        bgm.GetComponent<AudioSource>().volume = value;
     }
 }
