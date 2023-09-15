769d4d2303d6cb4ac719627d7d0d91056287e901
3240765224 <1530834198@qq.com>
2023-09-15 17:12:28 +0800

    新增npc交互显示

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.UI;
+
+public class Dialogue : MonoBehaviour
+{
+    public GameObject player;
+
+    public GameObject dialogueUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("123");
+            dialogueUI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            dialogueUI.SetActive(false);
+        }
+    }
+}
