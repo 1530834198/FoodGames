a8676f6e02723b02585a916dbefd0e41c9bd9cb0
3240765224 <1530834198@qq.com>
2023-09-18 00:02:56 +0800

    数据库存储方法ScriptableObject

---
+++ Assets/Inventory/InventoryScript/Item.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Item",menuName = "Inventory/New Item")]
+public class Item : ScriptableObject
+{
+
+    public string itemName;//物品名称
+    public Sprite itemImage;//物品图片
+    public int itemHeld;//物品数量
+    [TextArea] public string itemInfo;//物品信息
+
+    public bool equip;//是否可装备
+}
---
+++ Assets/Inventory/InventoryScript/ItemOnWorld.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemOnWorld : MonoBehaviour
+{
+    public Item thisItem;//物品
+    public Inventory playerInventory;//背包
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            AddNewItem();
+            Destroy(gameObject);
+        }
+    }
+
+    public void AddNewItem()
+    {
+        //如果物品不在背包里面，就增加进去
+        if (!playerInventory.itemsList.Contains(thisItem))
+        {
+            playerInventory.itemsList.Add(thisItem);
+        }
+        // else
+        // {
+        //     //如果在就增加数量
+        //     thisItem.itemHeld += 1;
+        // }
+    }
+}
